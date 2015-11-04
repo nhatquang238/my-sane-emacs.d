7c65e1483c8babe504f40f90bd238fcc32003826
Co <co@99.co>
2015-11-04 09:15:20 +0000

    ensure no tab upon saving

--- my-noexternals.el
+++ my-noexternals.el
@@ -14,6 +14,7 @@
 (add-to-list 'completion-styles 'initials t)
 
 ;; tab config
-(setq-default indent-tabs-mode nil)
 (custom-set-variables '(tab-stop-list (quote (4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100 104 108 112 116 120))))
 (setq-default tab-width 4)
+(setq-default indent-tabs-mode nil)
+(add-hook 'before-save-hook 'untabify)
